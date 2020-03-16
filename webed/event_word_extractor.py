4c1862f94429e36f01fe729fc9ab41908dc17a67
HHansi <h.hansi1991@gmail.com>
2020-03-16 22:38:54 +0000

    010: Added event word extractor

---
+++ webed/event_word_extractor.py
@@ -0,0 +1,33 @@
+# Created by Hansi at 3/16/2020
+import os
+
+import numpy as np
+
+from algo.utils.matrix_calculation import sort_matrix_values
+from algo.utils.vocabulary_calculation import get_word_list_by_word_pairs
+
+from utils.file_utils import write_list_to_text_file, delete_create_folder
+
+
+# n - word count (number of top words needed as event words)
+def get_event_words(event_windows, result_folder, n=None):
+    # delete if there already exist a folder and create new folder
+    delete_create_folder(result_folder)
+
+    for event_window in event_windows:
+        time_window = event_window.time_window
+        diff_ut_matrix = event_window.diff_ut_matrix
+        vocab = event_window.vocab
+
+        sim_word_pairs = sort_matrix_values(vocab, np.asarray(diff_ut_matrix), descending=True,
+                                            non_zeros_only=True)
+        word_pairs = np.array(sim_word_pairs)[:, 1]
+        word_list = get_word_list_by_word_pairs(word_pairs)
+
+        if n:
+            event_words = word_list[:n]
+        else:
+            event_words = word_list
+
+        result_file_path = os.path.join(result_folder, time_window + '.txt')
+        write_list_to_text_file(event_words, result_file_path)
