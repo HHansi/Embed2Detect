39436d31d4881045e4bdec3b4c9c1c3d5c829ce8
HHansi <h.hansi1991@gmail.com>
2020-03-16 22:30:58 +0000

    003: Added project configs

---
+++ project_config.py
@@ -0,0 +1,25 @@
+# Created by Hansi at 3/16/2020
+
+# word embedding configs
+model_type = 'sg'
+min_word_count = 1
+vector_size = 100
+window_size = 5
+
+# performance configs
+workers = 2
+
+# general configs
+preprocess = ['rm-punct', 'rm-stop_words']
+aggregation_method = 'max'
+
+# file path configs
+data_folder = 'data'
+preprocessed_data_folder = 'preprocessed_data'
+data_window_folder = 'data_windows'
+data_stats_folder = 'data_stats'
+word_embedding_folder = 'word_embedding'
+evaluation_results_folder = 'eval_results'
+
+resource_folder_path = '../resources'
+results_folder_path = '../results'
